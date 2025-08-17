6b3c24d5491ec6bb62b51c558b95986c38ca4c40
Kevin Lakotko <klakotko@gmail.com>
2025-08-17 11:46:54 -0400

    chore: remove console log and clean up typing

--- src/main.tsx
+++ src/main.tsx
@@ -5,15 +5,16 @@ import { atom, createStore, Provider } from 'jotai';
 import { listen } from '@tauri-apps/api/event';
 import { invoke } from '@tauri-apps/api/core';
 
-const store = createStore();
-export const pdfAtom = atom<{ name: string; pages: number; size: number }[]>([]);
+type PdfDetails = { name: string; pages: number; size: number };
 
-listen('folder-processed', (event) => {
-  const pdfMessage = event.payload as Array<{ name: string; pages: number; size: number }>;
+const store = createStore();
+export const pdfAtom = atom<PdfDetails[]>([]);
+
+listen<PdfDetails[]>('folder-processed', (event) => {
+  const pdfMessage = event.payload;
   store.set(pdfAtom, pdfMessage);
 })
   .then((cb) => {
-    console.log('invoking');
     invoke('frontend_ready');
 
     return cb;
