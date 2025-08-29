2164d5c228271db0a2087d6e1e54ac628b154176
Kevin Lakotko <klakotko@gmail.com>
2025-08-29 11:09:52 -0400

    refactor: use a string to avoid the switch

--- src/components/heading/Heading.tsx
+++ src/components/heading/Heading.tsx
@@ -12,27 +12,26 @@ type HeadingProps = {
   children: ReactNode;
 };
 
-export const Heading = ({ align = 'left', weight = 'normal', level, children }: HeadingProps) => {
-  const classes = classNames({
-    'text-center': align === 'center',
-    'text-left': align === 'left',
-    'text-right': align === 'right',
-    'font-bold': weight === 'bold',
-    'font-normal': weight === 'normal',
-    'text-3xl': level === '1',
-    'text-2xl': level === '2',
-    'text-xl': level === '3',
-    'text-l': level === '4',
-  });
+type CustomHeadingElement = Extract<keyof JSX.IntrinsicElements, 'h1' | 'h2' | 'h3' | 'h4'>;
 
-  switch (level) {
-    case '1':
-      return <h1 className={classes}>{children}</h1>;
-    case '2':
-      return <h2 className={classes}>{children}</h2>;
-    case '3':
-      return <h3 className={classes}>{children}</h3>;
-    case '4':
-      return <h4 className={classes}>{children}</h4>;
-  }
+export const Heading = ({ align = 'left', weight = 'normal', level, children }: HeadingProps) => {
+  const H = `h${level}` as CustomHeadingElement;
+
+  return (
+    <H
+      className={classNames({
+        'text-center': align === 'center',
+        'text-left': align === 'left',
+        'text-right': align === 'right',
+        'font-bold': weight === 'bold',
+        'font-normal': weight === 'normal',
+        'text-3xl': level === '1',
+        'text-2xl': level === '2',
+        'text-xl': level === '3',
+        'text-lg': level === '4',
+      })}
+    >
+      {children}
+    </H>
+  );
 };
